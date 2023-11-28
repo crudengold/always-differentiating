a60158efdc34b726632e2464d9b2d09f85b6ca4e
Alex Cruden <alexscruden@gmail.com>
2023-11-28 20:53:29 +0000

    already done some work but going to start pushing now

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module AlwaysDifferentiating
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.active_job.queue_adapter = :delayed_job
   end
 end
