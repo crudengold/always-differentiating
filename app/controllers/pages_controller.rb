57e3998153bda5768983c50a77871465288596e4
Alex Cruden <alexscruden@gmail.com>
2023-12-19 20:19:40 +0000

    took hardcoded gw out

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -22,7 +22,6 @@ class PagesController < ApplicationController
     @update_time = SelectedByStat.last.created_at
     @illegal_players = SelectedByStat.where("selected_by > ? AND gameweek = ?", 10, @gameweek).order(selected_by: :desc)
     @penalties = Penalty.where("gameweek = ?", @gameweek)
-    @penalties = []
     @penalty_players = Penalty.distinct.pluck(:player_id)
   end
 
