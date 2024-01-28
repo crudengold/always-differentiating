37fdd4194073334a360d85bdaa6d99d47980e2f9
Alex Cruden <alexscruden@gmail.com>
2024-01-28 14:07:41 +0000

    it's alive

---
+++ app/javascript/controllers/penalties_clipboard_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="penalties-clipboard"
+export default class extends Controller {
+  static targets = ["player", "copyButton"]
+
+  connect() {
+    // console.log("penalties connected")
+    // this.playerTargets.forEach ((player) => {
+    //   console.log(player.innerText)
+    // })
+  }
+
+  penaltiesList() {
+    penalties = []
+    this.playerTargets.forEach ((player) => {
+      penalties.push(player.innerText)
+    })
+    navigator.clipboard.writeText(penalties.join("\n\n------\n\n"))
+    // console.log(penalties.join("\n\n------\n\n"))
+  }
+}
