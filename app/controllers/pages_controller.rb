cd947a995c6e89838b5bcea9b4051e3d02287a35
Alex Cruden <alexscruden@gmail.com>
2024-02-05 14:53:01 +0000

    moved database to postgres

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -33,6 +33,10 @@ class PagesController < ApplicationController
   def new
   end
 
+  def transfers
+    @transfers = get_transfers
+  end
+
   def position(num)
     # converts the element type number into a position
     if num == 1
@@ -46,7 +50,40 @@ class PagesController < ApplicationController
     end
   end
 
-  def get_shirts()
-
+  def get_transfers
+    transfers = {}
+    # get the current gameweek
+    general_url = "https://fantasy.premierleague.com/api/bootstrap-static/"
+    user_serialized = URI.open(general_url).read
+    all_data = JSON.parse(user_serialized)
+    # get the current gameweek
+    all_data["events"].each do |num|
+      if num["is_current"] == true
+        @gameweek = num["id"]
+      end
+    end
+    # for each fpl team
+    Fplteam.all.each do |team|
+    # add team to transfers hash with empty hash as value
+      transfers[team.entry_name] = {in: [], out: []}
+    # get last week's picks
+      last_week = team.picks.where("gameweek = ?", @gameweek - 1)
+    # get this week's picks
+      this_week = team.picks.where("gameweek = ?", @gameweek)
+    # compare the two
+      raise
+      last_week.each do |pick|
+    # if a player is in last week's picks but not this week's, add to team hash as key
+        if this_week.where("player_id = ?", pick.player_id).empty?
+          transfers[team][:out] << pick.player_id
+        end
+    # if a player is in this week's picks but not last week's, add to team hash as value
+        if last_week.where("player_id = ?", pick.player_id).empty?
+          transfers[team][:in] << pick.player_id
+        end
+      end
+    end
+    # return transfers hash
+    return transfers
   end
 end
