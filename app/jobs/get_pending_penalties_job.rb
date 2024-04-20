048a27cde8961d91c3045e79433bb97b4f2cb36a
Alex Cruden <alexscruden@gmail.com>
2024-04-20 12:05:19 +0100

    pending penalties are fixed now

--- app/jobs/get_pending_penalties_job.rb
+++ app/jobs/get_pending_penalties_job.rb
@@ -9,41 +9,11 @@ class GetPendingPenaltiesJob < ApplicationJob
     all_data = ApiJson.new("https://fantasy.premierleague.com/api/bootstrap-static/").get
     gameweek = Gameweek.new(all_data, "next").gw_num
 
-    illegal_players = {}
+    illegal_players = Player.illegal_players(gameweek).sort_by {|_key, value| value}.reverse.to_h
+    last_weeks_free_hitters = Fplteam.last_weeks_free_hitters(gameweek)
 
-    Player.all.each do |player|
-      if !player.past_ownership_stats[gameweek.to_s].nil? && player.past_ownership_stats[gameweek.to_s] >= 15
-        illegal_players[player] = player.past_ownership_stats[gameweek.to_s]
-      end
-    end
-
-    illegal_players = illegal_players.sort_by {|_key, value| value}.reverse
-
-    last_weeks_free_hitters = Fplteam.all.select do |fplteam|
-      fplteam.free_hit?(fplteam.entry, gameweek - 1)
-    end
-
-    # Check non free-hitters
-    Pick.where(gameweek: gameweek - 1).each do |pick|
-      if illegal_players.include?(pick.player) && !last_weeks_free_hitters.include?(pick.fplteam)
-        Penalty.create(player: pick.player, fplteam: pick.fplteam, gameweek: gameweek)
-      end
-    end
-
-    # Check free-hitters
-    Pick.where(gameweek: gameweek - 2).each do |pick|
-      if illegal_players.include?(pick.player) && last_weeks_free_hitters.include?(pick.fplteam)
-        Penalty.create(player: pick.player, fplteam: pick.fplteam, gameweek: gameweek)
-      end
-    end
-
-    last_weeks_free_hitters.each do |fplteam|
-      # if the team had a warning last week, create the same warning for this week, unless it already exists
-      Penalty.where(fplteam: fplteam, gameweek: gameweek - 1).each do |warning|
-        unless Penalty.where(player: warning.player, fplteam: warning.fplteam, gameweek: gameweek).exists?
-          Penalty.create(player: warning.player, fplteam: warning.fplteam, gameweek: gameweek)
-        end
-      end
-    end
+    Penalty.create_for_non_free_hitters(gameweek, illegal_players, last_weeks_free_hitters)
+    Penalty.create_for_free_hitters(gameweek, illegal_players, last_weeks_free_hitters)
+    Penalty.create_for_previous_warnings(gameweek, last_weeks_free_hitters)
   end
 end
